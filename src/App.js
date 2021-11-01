d36bc7e1ee19a30697c2019a7613e945d8885e98
Belen Romero <romero.abelen@gmail.com>
2021-11-01 19:59:21 -0300

    routes

--- src/App.js
+++ src/App.js
@@ -1,17 +1,29 @@
+import { BrowserRouter, Switch, Route } from "react-router-dom";
 import "./App.css";
 import { Navbar } from "./components/NavBar/NavBar";
+import { ItemListContainer } from "./components/ItemListContainer/ItemListContainer";
 import { ItemDetailContainer } from "./components/ItemDetailContainer/ItemDetailContainer";
 
 function App() {
   return (
-    <div className="App">
+    <BrowserRouter>
       <header className="App-header">
         <Navbar />
       </header>
-      <main>
-        <ItemDetailContainer />
-      </main>
-    </div>
+      <Switch>
+        <Route exact path="/">
+          <main className="App">
+            <ItemListContainer />
+          </main>
+        </Route>
+        <Route path="/categoria/:categoriaId">
+          <ItemListContainer />
+        </Route>
+        <Route exact path="/item/:itemId">
+          <ItemDetailContainer />
+        </Route>
+      </Switch>
+    </BrowserRouter>
   );
 }
 
