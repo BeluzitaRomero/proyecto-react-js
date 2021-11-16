580ed363e743a2a49869c607f19035dd7a305c8f
Belen Romero <romero.abelen@gmail.com>
2021-11-16 18:33:33 -0300

    estilo navbar e imagenes cart

--- src/components/CartWidget/CartWidget.jsx
+++ src/components/CartWidget/CartWidget.jsx
@@ -1,16 +1,31 @@
 import "./CartWidget.css";
-import cart from "../../assets/shoppingCart.png";
+import cartFull from "../../assets/shoppingCart.png";
+import empty from "../../assets/cart-vacio-color.png";
+import { useCartContext } from "../../contexts/CartContext";
 
 export const CartWidet = (props) => {
+  const { cart } = useCartContext;
+  console.log("carrito", cart);
+
   return (
-    <>
-      <img
-        className="cart"
-        src={cart}
-        alt="shooping cart"
-        width="60"
-        height="60"
-      />
-    </>
+    <div>
+      {cart === undefined ? (
+        <img
+          className="cart"
+          src={empty}
+          alt="shooping cart"
+          width="60"
+          height="60"
+        />
+      ) : (
+        <img
+          className="cart"
+          src={cartFull}
+          alt="shooping cart"
+          width="60"
+          height="60"
+        />
+      )}
+    </div>
   );
 };
--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,32 +1,49 @@
-import "./NavBar.css";
 import astro from "../../assets/astro.png";
 import { CartWidet } from "../CartWidget/CartWidget";
 import { Link } from "react-router-dom";
-import { Container, Navbar, Nav} from "react-bootstrap";
+import { Container, Navbar, Nav } from "react-bootstrap";
+import "./NavBar.css";
 
 export function NavBar() {
   return (
-    <Navbar className="d-flex justify-content-center" collapseOnSelect expand="lg" bg="dark" variant="dark" >
-      <Navbar.Brand className="logo-moon mx-0" as={Link} to="/">Tienda Moon<img alt="logo" width="80" height="80" src={astro}/></Navbar.Brand>
-  <Container >
-  
-  <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-  <Navbar.Collapse id="responsive-navbar-nav">
-    <Nav className="me-auto">
-      <Nav.Link as={Link} to="/categoria/new">New</Nav.Link>
-      <Nav.Link as={Link} to="/categoria/bebes">Bebés</Nav.Link>
-      <Nav.Link as={Link} to="/categoria/niñxs">Niñxs</Nav.Link>
-      <Nav.Link as={Link} to="/categoria/+10">Niñxs+10</Nav.Link>
-      <Nav.Link as={Link} to="/categoria/sale">SALE!</Nav.Link>
-    </Nav>
-    
-  </Navbar.Collapse>
-    <div className="cart-div">
-      <Link to="/cart">
-        <CartWidet/>
-      </Link>
-    </div>
-  </Container>
-</Navbar>
+    <Navbar
+      className="d-flex justify-content-center navbar-style"
+      collapseOnSelect
+      expand="lg"
+      bg="dark"
+      variant="dark"
+    >
+      <Navbar.Brand className="logo-moon mx-0" as={Link} to="/">
+        <h1 className="logo-titulo">Tienda Moon</h1>
+        <img alt="logo" width="80" height="80" src={astro} />
+      </Navbar.Brand>
+      <Container>
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse id="responsive-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link as={Link} to="/categoria/new">
+              New
+            </Nav.Link>
+            <Nav.Link as={Link} to="/categoria/bebes">
+              Bebés
+            </Nav.Link>
+            <Nav.Link as={Link} to="/categoria/niñxs">
+              Niñxs
+            </Nav.Link>
+            <Nav.Link as={Link} to="/categoria/+10">
+              Niñxs+10
+            </Nav.Link>
+            <Nav.Link as={Link} to="/categoria/sale">
+              SALE!
+            </Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+        <div className="cart-div">
+          <Link to="/cart">
+            <CartWidet />
+          </Link>
+        </div>
+      </Container>
+    </Navbar>
   );
 }
