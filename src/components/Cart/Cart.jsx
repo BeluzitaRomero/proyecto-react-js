9a003f842a985bfb082d727fedd6c51ec60da299
Belen Romero <romero.abelen@gmail.com>
2021-11-16 23:42:50 -0300

    estilo carrito vacio

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -3,6 +3,7 @@ import { Button } from "react-bootstrap";
 import { Link } from "react-router-dom";
 import { useCartContext } from "../../contexts/CartContext";
 import { CartItem } from "../CartItem/CartItem";
+import globos from "../../assets/globos.png";
 
 export function Cart() {
   const { cart, clear, removeItem, removeProduct } = useCartContext();
@@ -13,17 +14,27 @@ export function Cart() {
     <div className="cart">
       <h1 className="titulo">Productos en carrito</h1>
       <div className="productos-en-carrito">
-        {cart.length
-          ? cart.map((inCart) => (
-              <CartItem
-                key={inCart.id}
-                item={inCart}
-                removeItem={() => removeItem(inCart)}
-                removeProd={() => removeProduct(inCart)}
-                total={inCart.precio * inCart.cantidad}
-              />
-            ))
-          : "Tu carrito está vacío! :("}
+        {cart.length ? (
+          cart.map((inCart) => (
+            <CartItem
+              key={inCart.id}
+              item={inCart}
+              removeItem={() => removeItem(inCart)}
+              removeProd={() => removeProduct(inCart)}
+              total={inCart.precio * inCart.cantidad}
+            />
+          ))
+        ) : (
+          <div>
+            <img
+              className="img-empty"
+              src={globos}
+              alt="astronauta con globos"
+            />
+            <h2>Tu carrito está vacío 😕</h2>
+            <p>¡Te esperamos nuevamente por nuestra seccion de compras! 🚀</p>
+          </div>
+        )}
       </div>
       {cart.length ? (
         <Button className="btn-clear" onClick={clear}>
