6dbab1f429ec808baec32bf43f85786511457cd5
Belen Romero <romero.abelen@gmail.com>
2021-11-23 01:43:32 -0300

    formulario cliente

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -1,19 +1,31 @@
 import "./Cart.css";
-import { Button } from "react-bootstrap";
+import { Button} from "react-bootstrap";
 import { Link } from "react-router-dom";
 import { useCartContext } from "../../contexts/CartContext";
 import { CartItem } from "../CartItem/CartItem";
 import globos from "../../assets/globos.png";
+import { CartCheckOut } from "../CartCheckout/CartCheckout";
+import { useState } from "react";
+
 
 export function Cart() {
   const { cart, clear, removeItem, removeProduct } = useCartContext();
-
-  console.log(cart);
+ 
+  const [client, setClient] = useState({name: "", email: "", tel: ""
+  })
 
   const totalAPagar = cart.reduce(
     (counter, item) => counter + item.precio * item.cantidad,
     0
   );
+
+  const finalizarCompra = (e) => {
+    e.preventDefault();
+    setClient({...client, item: cart, total: totalAPagar})
+    //aca deberia hacer en envio a la base de datos
+    console.log(client)
+
+  }
 
   return (
     <div className="cart">
@@ -48,7 +60,16 @@ export function Cart() {
             Vaciar carrito
           </Button>
           <p className="total-a-pagar">Total a pagar: ${totalAPagar}</p>
-          <Button>Finalizar compra</Button>
+          <form id="my-form" onSubmit={finalizarCompra}>
+            <label>Nombre</label>
+            <CartCheckOut setClient={setClient} name="name" client={client}/>
+            <label>Email</label>
+            <CartCheckOut setClient={setClient} name="email" client={client}/>
+            <label>Telefono</label>
+            <CartCheckOut setClient={setClient} name="tel" client={client}/>
+            <Button type="submit">Finalizar compra</Button>
+          </form>
+          
         </div>
   
       ) : (
@@ -56,6 +77,7 @@ export function Cart() {
           <Button className="btn-volver-shop">Volver a seccion de compra</Button>
         </Link>
       )}
+      
     </div>
   );
 }
