4a642c91d87fda9c791ca1dc7bb55efbe48c743e
Belen Romero <romero.abelen@gmail.com>
2021-12-08 16:14:20 -0300

    pago

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -1,67 +1,115 @@
-import "./Form.css"
-import { Button} from "react-bootstrap";
+import "./Form.css";
+import { Button } from "react-bootstrap";
 import { useState } from "react";
 import { useCartContext } from "../../contexts/CartContext";
-import {getFirestore} from "../../firebase/index"
+import { getFirestore } from "../../firebase/index";
 import { collection, addDoc, doc, updateDoc } from "@firebase/firestore";
+import swal from "sweetalert";
 
-export function Form ({total}) {
+export function Form({ total }) {
   const { cart, setCart } = useCartContext();
 
-  const [client, setClient] = useState({name: "", email: "", tel: ""
-  })
- 
-  const handleClientOnChange = (e) =>{
-    setClient({...client, [e.target.name]: e.target.value  })
-  }
+  const [client, setClient] = useState({
+    name: "",
+    email: "",
+    tel: "",
+    tarjeta: "",
+  });
 
-
-  
+  const handleClientOnChange = (e) => {
+    setClient({ ...client, [e.target.name]: e.target.value });
+  };
 
   const finalizarCompra = (e) => {
     e.preventDefault();
     const order = {
       cliente: client,
       items: cart,
-      total:total
-    }
+      total: total,
+    };
 
+    const db = getFirestore();
+    const ordersCollection = collection(db, "orders");
 
-    const db= getFirestore();
-    const ordersCollection = collection(db, "orders")
-
-    addDoc(ordersCollection, order).then(({id})=> {
-      console.log(id);
-      alert(`Muchas gracias por comprar en Tienda Moon ♥ Su codigo de compra es: ${id}`)
+    addDoc(ordersCollection, order).then(({ id }) => {
+      swal(`Tu codigo de referencia de compra es: ${id}. 
+    Gracias por elegir Tienda Moon ♥`);
     });
-
 
     // update stock
     cart.forEach((item) => {
-      const docRef = doc( db, "items", item.id);
-      updateDoc(docRef, {stock: item.stock - item.cantidad});
-    })
+      const docRef = doc(db, "items", item.id);
+      updateDoc(docRef, { stock: item.stock - item.cantidad });
+    });
 
-
-    
-    setClient({name: "", email: "", tel: ""})
+    setClient({ name: "", email: "", tel: "" });
     setCart([]);
-  }
+  };
 
-  
+  return (
+    <>
+      <form id="my-form" onSubmit={finalizarCompra}>
+        <div className="grid-form">
+          <input
+            placeholder="Nombre"
+            type="text"
+            name="name"
+            value={client.name}
+            onChange={handleClientOnChange}
+            required
+          />
 
-    return (
-        <>
-        <form id="my-form" onSubmit={finalizarCompra}>
-            <label>Nombre</label>
-            <input type="text" name="name" value={client.name} onChange={handleClientOnChange} required/>
-            <label>Email</label>
-            <input type="email" name="email" value={client.email} onChange={handleClientOnChange} required/>
-            <label>Telefono</label>
-            <input type="number" name="tel" value={client.tel} onChange={handleClientOnChange} required/>
-            <Button type="submit" disabled={!(client.name && client.email && client.tel)}>Finalizar compra</Button>     
-          </form>
-        
-        </>
-     )
+          <input
+            placeholder="Email@email.com"
+            type="email"
+            name="email"
+            value={client.email}
+            onChange={handleClientOnChange}
+            required
+          />
+
+          <input
+            placeholder="Telefono"
+            type="number"
+            name="tel"
+            value={client.tel}
+            onChange={handleClientOnChange}
+            required
+          />
+
+          <input
+            placeholder="Numero de tarjeta"
+            type="number"
+            name="tarjeta"
+            value={client.tarjeta}
+            onChange={handleClientOnChange}
+            required
+          />
+          <label>Debito</label>
+          <input
+            type="radio"
+            name="card"
+            value={client.card}
+            onChange={handleClientOnChange}
+            required
+          />
+          <label>Credito</label>
+          <input
+            type="radio"
+            name="card"
+            value={client.card}
+            onChange={handleClientOnChange}
+            required
+          />
+        </div>
+
+        <Button
+          type="submit"
+          disabled={!(client.name && client.email && client.tel)}
+        >
+          Finalizar compra
+        </Button>
+      </form>
+    </>
+  );
 }
